53d06feb3acae2f1c568a9e18cf080820acfd1a0
Omar <omarzahir12@gmail.com>
2021-05-26 21:55:10 -0400

    added typing effect

--- js/main.js
+++ js/main.js
@@ -5,6 +5,52 @@
  * ------------------------------------------------------------------- 
  */ 
 
+ const typedTextSpan = document.querySelector(".typed-text");
+ const cursorSpan = document.querySelector(".cursor");
+ 
+ const textArray = ["Omar Zahir", "Fun", "Creative", "Hardworking", "Exhausted"];
+ const typingDelay = 200;
+ const erasingDelay = 100;
+ const newTextDelay = 2000; // Delay between current and next text
+ let textArrayIndex = 0;
+ let charIndex = 10;
+ 
+ let first = true;
+
+ function type() {
+   if (charIndex < textArray[textArrayIndex].length) {
+	if (first) {
+		typedTextSpan.textContent = "Omar Zahir";
+	} else{
+		typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+	} 
+	 charIndex++;
+	 setTimeout(type, typingDelay);
+   } 
+   else {
+	   setTimeout(erase, newTextDelay);
+   }
+ }
+ 
+ function erase() {
+	 if (charIndex > 0) {
+	 first = false;
+	 typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+	 charIndex--;
+	 setTimeout(erase, erasingDelay);
+   } 
+   else {
+	 textArrayIndex++;
+	 if(textArrayIndex>=textArray.length) textArrayIndex=0;
+	 setTimeout(type, typingDelay + 1100);
+   }
+ }
+ 
+//  document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+typedTextSpan.textContent = "Omar Zahir";      
+if(textArray.length) setTimeout(type, newTextDelay + 250);
+//  });
+
 (function($) {
 
 	"use strict";
