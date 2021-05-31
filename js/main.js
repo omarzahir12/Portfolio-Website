e83db7f3e100442e7ac92034fa0b778e1aee80a1
Omar <omarzahir12@gmail.com>
2021-05-31 17:17:09 -0400

    added more projects

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@
  const typedTextSpan = document.querySelector(".typed-text");
  const cursorSpan = document.querySelector(".cursor");
  
- const textArray = ["Omar Zahir", "Fun", "Creative", "Hardworking", "Exhausted"];
+ const textArray = ["Omar Zahir", "Innovative", "Passionate", "Creative", "Hardworking"];
  const typingDelay = 200;
  const erasingDelay = 100;
  const newTextDelay = 2000; // Delay between current and next text
